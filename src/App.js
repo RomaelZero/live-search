5340145adfe5b8918287a03e18dcbd3109d898e0
Romael Singh Raina <romaelwork6000@gmail.com>
2024-05-07 14:25:44 +0530

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Table from "react-bootstrap/Table";
+import { data } from "./data.js";
 
 function App() {
+  const [searchWord, setSearchWord] = useState('');
+
+  // Filter data based on searchWord
+  const filteredData = data.filter(entry => {
+    const searchText = searchWord.toLowerCase();
+    return (
+      entry.first_name.toLowerCase().includes(searchText) ||
+      entry.last_name.toLowerCase().includes(searchText) ||
+      entry.email.toLowerCase().includes(searchText) ||
+      entry.phone.toLowerCase().includes(searchText)
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ textAlign: "center", width: "50%", margin: "10px auto" }}>Contact Keeper</h1>
+
+      <Form style={{ width: "70%", margin: "10px auto" }}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Control
+            type="text"
+            placeholder="Search Contacts"
+            value={searchWord}
+            onChange={(e) => setSearchWord(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+
+      <div style={{ width: "70%", margin: "10px auto" }}>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((entry) => (
+              <tr key={entry.id}>
+                <td>{entry.id}</td>
+                <td>{entry.first_name}</td>
+                <td>{entry.last_name}</td>
+                <td>{entry.email}</td>
+                <td>{entry.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </>
   );
 }
 
